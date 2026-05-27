899071ad67d99b4640402a53192ae791db8602e9
Yamt14 <yamt14@hotmail.com>
2026-05-26 22:37:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -55,22 +55,18 @@ try:
     zero_gamma_val = preco_spot - 25
 
 # --- BLOCO SUPERIORES DE MÉTRICAS ---
-    col1, col2, col3, col4 = st.columns(4, gap="small")
-    
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
-        st.markdown("<div style='font-size: 13px; color: #aaa; margin-bottom: 5px;'>MNQ PREÇO ATUAL</div>", unsafe_allow_html=True)
+        st.subheader("MNQ PREÇO ATUAL")
         st.metric(label="", value=f"{preco_spot:,.2f}")
-        
     with col2:
-        st.markdown("<div style='font-size: 13px; color: #aaa; margin-bottom: 5px;'>CALL WALL (Resistência)</div>", unsafe_allow_html=True)
+        st.subheader("CALL WALL (Resistência)")
         st.metric(label="", value=f"{call_wall_val:,.2f}")
-        
     with col3:
-        st.markdown("<div style='font-size: 13px; color: #aaa; margin-bottom: 5px;'>PUT WALL (Suporte)</div>", unsafe_allow_html=True)
+        st.subheader("PUT WALL (Suporte)")
         st.metric(label="", value=f"{put_wall_val:,.2f}")
-        
     with col4:
-        st.markdown("<div style='font-size: 13px; color: #aaa; margin-bottom: 5px;'>ZERO GAMMA (Pivô)</div>", unsafe_allow_html=True)
+        st.subheader("ZERO GAMMA (Pivô)")
         st.metric(label="", value=f"{zero_gamma_val:,.2f}")
     st.markdown("---")
 
